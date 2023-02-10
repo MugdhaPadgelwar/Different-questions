f7f8dd5fd1a535a81ec6553e88a3899f1a160c26
MugdhaPadgelwar <101932627+MugdhaPadgelwar@users.noreply.github.com>
2023-02-10 18:57:49 +0530

    initial commit

--- pattern1.java
+++ pattern1.java
@@ -2,7 +2,18 @@ public class pattern1 {
     public static void main(String[] args)
     {
           //pattern(4);
-          pattern3(5);
+          pattern4(5);
+    }
+    public static void pattern4(int n)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
     }
 
     public static void pattern3(int n)
